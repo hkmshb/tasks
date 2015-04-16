f6b5f2bd1fc40b2b49b6a785685e47811d4df928
hkmshb <hkmshb@gmail.com>
2015-04-16 13:31:21 +0100

    First commit: First FT and basic Django config

---
+++ functional_tests.py
@@ -0,0 +1,10 @@
+"""
+Defines functional tests for tasks.
+"""
+from selenium import webdriver
+
+
+browser = webdriver.Firefox()
+browser.get('http://localhost:8000')
+
+assert 'Django' in browser.title
